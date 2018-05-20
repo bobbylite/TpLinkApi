de1bedb0260ae1e501eb23d77b72de0beff6d949
Robert Luisi <bobby.luisi@gmail.com>
2018-05-20 18:05:27 -0400

    Added react Ant.Design

---
+++ views/tplinkui/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import TpLink from './TpLink.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TpLink/>
+      </div>
+    );
+  }
+}
+
+export default App;
